45c8f7e19b00dbdc77497d08471628fd5b6ba072
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-05-04 12:48:25 -0400

    Add project files.

---
+++ CRUDBlazor/Data/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDBlazor.Data
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
---
+++ CRUDBlazor/Services/ICustomerService.cs
@@ -0,0 +1,17 @@
+﻿using CRUDBlazor.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDBlazor.Services
+{
+    public interface ICustomerService
+    {
+        Task<List<Customer>> Get();
+        Task<Customer> Get(int id);
+        Task<Customer> Add(Customer customer);
+        Task<Customer> Update(Customer customer);
+        Task<Customer> Delete(int id);
+    }
+}
